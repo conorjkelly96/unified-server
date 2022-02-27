ed4df80a1936817fea75ae95b48d2bcbb0c99ba6
Conor Kelly <conorjkelly96@gmail.com>
2022-02-27 14:43:18 +0000

    index page for models

---
+++ src/models/index.js
@@ -0,0 +1,11 @@
+const ForumBoard = require("./Forumboard");
+const User = require("./User");
+const Item = require("./Item");
+const Job = require("./Job");
+
+module.exports = {
+  ForumBoard,
+  User,
+  Item,
+  Job,
+};
