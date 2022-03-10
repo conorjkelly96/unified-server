95998dd2df7621693a646d864a9511ed115be4fc
Kayle Rieger Patton <kayle.patton22@gmail.com>
2022-03-10 16:07:19 +0000

    forumReply schema

---
+++ src/models/ForumPost.js
@@ -0,0 +1,42 @@
+const { model, Schema } = require("mongoose");
+const { formatDate } = require("../utils");
+
+// import reaction schema
+const replies = require("./ForumReply");
+
+const forumPostSchema = {
+  postText: {
+    type: String,
+    required: true,
+    maxLength: 2000,
+  },
+
+  username: {
+    type: String,
+    required: true,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: formatDate,
+  },
+
+  //   replies is subdocument schema
+  replies: [replies],
+};
+
+const schema = new Schema(forumPostSchema, {
+  timestamps: true,
+  id: true,
+  toJSON: { getters: true },
+});
+
+// virtual to total the reply count
+schema.virtual("replyCount").get(function () {
+  return this.replies.length;
+});
+
+const ForumPost = model("forumPost", schema);
+
+module.exports = ForumPost;
